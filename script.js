01b32d57628eee18e5ee2ce8ab088fb4f44c09a7
vlb834 <32312712+vlb834@users.noreply.github.com>
2018-01-21 13:44:02 +0000

    Parsing of JSON data into variables for HTML - detailed weather descriptions completed.

--- script.js
+++ script.js
@@ -3,6 +3,62 @@
 // https://www.sitepoint.com/html5-geolocation/ - Code from this site.
 // http://youmightnotneedjquery.com/ 
 
+
+// GLOBAL VARIABLES //
+const tempC = "&#176C";
+const tempF = "&#176F";
+let tempUnit = tempC;
+let geolocation = '';
+let temp = '';
+let tempMax = '';
+let tempMin = '';
+//let icon = ;
+let humidity = '';
+let windSpeed = '';
+//let windDirection = 
+let detailedDescription = '';
+let sunRise = '';
+let sunSet = '';
+
+function defineWeatherVariables(obj) {
+    geolocation = obj['name'];
+    temp = obj['main']['temp'] + tempUnit;
+    tempMax = obj['main']['temp_max'] + tempUnit;
+    tempMin = obj['main']['temp_min'] + tempUnit;
+    //icon = ;
+    humidity = 'Humidity: ' + obj['main']['humidity'] + '&#37';
+    windSpeed = 'Wind: ' + obj['wind']['speed'] + 'km/hr';
+    // windDirection = obj['wind']['deg'-.map(x => x['description'])
+    // sunRise =
+    // sunSet = 
+    if (obj['weather'].length > 1) {
+        let descriptions = obj['weather'].map(x => x['description']);
+        detailedDescription = 'Mix of ';
+        for (let x of descriptions) {
+            detailedDescription = detailedDescription.concat(x + ', ');
+        }
+        detailedDescription = detailedDescription.slice(0, detailedDescription.length - 2) + '.';
+    } else {
+        detailedDescription = obj['weather']['description'];
+        detailedDescription = detailedDescription.charAt(0).toUpperCase() + detailedDescription.slice(1);
+    }
+}
+
+
+function displayCurrentWeather(obj) {
+    defineWeatherVariables(obj);
+    document.getElementById("location").innerHTML = geolocation;
+    document.getElementById("temperature").innerHTML = temp;
+    document.getElementById("description").innerHTML = detailedDescription;
+    document.getElementById("humidity").innerHTML = humidity;
+    document.getElementById("wind-speed").innerHTML = windSpeed;
+    // document.getElementById("wind-direction").innerHTML = windDirection;
+    // document.getElementById("sunrise").innerHTML = sunRise;
+    // document.getElementById("sunset").innerHTML = sunSet;
+
+}
+
+
 function getCurrentWeather(latitude, longitude) {
     var request = new XMLHttpRequest();
     request.open('GET', 'https://fcc-weather-api.glitch.me/api/current?lat=' + latitude + '&lon=' + longitude, true);
@@ -11,6 +67,7 @@ function getCurrentWeather(latitude, longitude) {
             // Success!
             var weatherData = JSON.parse(this.response);
             console.log(weatherData);
+            displayCurrentWeather(weatherData);
         } else {
             // We reached our target server, but it returned an error
         }
@@ -21,7 +78,7 @@ function getCurrentWeather(latitude, longitude) {
     request.send();
 }
 
-function showPosition(position) {
+function usePosition(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     getCurrentWeather(latitude, longitude);
@@ -44,13 +101,13 @@ function showError(error) {
     }
 }
 
-// JS FOR PAGE LOAD - retrieve location// 
+// JS FOR PAGE LOAD - retrieve HTML5 geolocation// 
 
 function run() {
 
     if (navigator.geolocation) {
         // Get the user's current position
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
+        navigator.geolocation.getCurrentPosition(usePosition, showError);
     } else {
         alert('Geolocation is not supported in your browser');
     }
@@ -60,7 +117,7 @@ function run() {
 // in case the document is already rendered
 if (document.readyState != 'loading') run();
 // modern browsers
-else if (document.addEventListener) document.addEventListener('DOMContentLoaded', run);
+else if (document.addEventListener) document.addEventListener('DOMContentLoaded', run());
 // IE <= 8
 else document.attachEvent('onreadystatechange', function () {
     if (document.readyState == 'complete') run();
