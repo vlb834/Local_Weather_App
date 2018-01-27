41e7479bdbb03ea51e8cf87c0cc1a1b9903aa66d
vlb834 <32312712+vlb834@users.noreply.github.com>
2018-01-27 18:20:54 +0000

    fixed code formatting

--- script.js
+++ script.js
@@ -85,92 +85,92 @@ function displayIcons(weather) {
                 img.src = "https://cdn.glitch.com/6e8889e5-7a72-48f0-a061-863548450de5%2F" + [x] + ".png?1499366021399";
                 document.getElementById("icon").appendChild(img);
             }
-        } 
+        }
     } else {
         let img = new Image();
         img.src = weather[0]['icon']; // check this works!!! 
         document.getElementById("icon").appendChild(img);
     }
-}    
+}
 
-    function displayCurrentWeather(weatherData) {
-        defineWeatherVariables(weatherData);
-        displayIcons(weatherData['weather']); // possible multiple icons for variable weather addressed by function
-        document.getElementById("location").innerHTML = geolocation;
-        document.getElementById("temperature").innerHTML = temp + tempUnit;
-        document.getElementById("description").innerHTML = detailedDescription;
-        document.getElementById("humidity").innerHTML = humidity;
-        document.getElementById("pressure").innerHTML = pressure;
-        document.getElementById("wind").innerHTML = windSpeed + ' ' + windDirection;
-        document.getElementById("sunrise").innerHTML = sunRise;
-        document.getElementById("sunset").innerHTML = sunSet;
-        document.getElementById("details").style.display = 'block';
-        document.getElementById("toggleTemp").style.display = 'block';
-        // document.getElementByItoggleTemppaper").style["background-image"] = 'linear-gradient(45deg, green, yellow)';
-        document.getElementById("wallpaper").style["background-image"] = gradient;
-    }
+function displayCurrentWeather(weatherData) {
+    defineWeatherVariables(weatherData);
+    displayIcons(weatherData['weather']); // possible multiple icons for variable weather addressed by function
+    document.getElementById("location").innerHTML = geolocation;
+    document.getElementById("temperature").innerHTML = temp + tempUnit;
+    document.getElementById("description").innerHTML = detailedDescription;
+    document.getElementById("humidity").innerHTML = humidity;
+    document.getElementById("pressure").innerHTML = pressure;
+    document.getElementById("wind").innerHTML = windSpeed + ' ' + windDirection;
+    document.getElementById("sunrise").innerHTML = sunRise;
+    document.getElementById("sunset").innerHTML = sunSet;
+    document.getElementById("details").style.display = 'block';
+    document.getElementById("toggleTemp").style.display = 'block';
+    // document.getElementByItoggleTemppaper").style["background-image"] = 'linear-gradient(45deg, green, yellow)';
+    document.getElementById("wallpaper").style["background-image"] = gradient;
+}
 
-    function getCurrentWeather(latitude, longitude) {
-        var request = new XMLHttpRequest();
-        request.open('GET', 'https://fcc-weather-api.glitch.me/api/current?lat=' + latitude + '&lon=' + longitude, true);
-        request.onload = function () {
-            if (this.status >= 200 && this.status < 400) {
-                // Success!
-                var weatherData = JSON.parse(this.response);
-                console.log(weatherData);
-                displayCurrentWeather(weatherData);
-            } else {
-                // We reached our target server, but it returned an error
-            }
-        };
-        request.onerror = function () {
-            // There was a connection error of some sort
-        };
-        request.send();
-    }
-
-    function usePosition(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        getCurrentWeather(latitude, longitude);
-    }
-
-    function showError(error) {
-        switch (error.code) {
-            case error.PERMISSION_DENIED:
-                alert("User denied the request for Geolocation.");
-                break;
-            case error.POSITION_UNAVAILABLE:
-                alert("Location information is unavailable.");
-                break;
-            case error.TIMEOUT:
-                alert("The request to get user location timed out.");
-                break;
-            case error.UNKNOWN_ERROR:
-                alert("An unknown error occurred.");
-                break;
-        }
-    }
-
-    // JS FOR PAGE LOAD - retrieve HTML5 geolocation// 
-
-    function run() {
-        if (navigator.geolocation) {
-            // Get the user's current position
-            navigator.geolocation.getCurrentPosition(usePosition, showError);
+function getCurrentWeather(latitude, longitude) {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://fcc-weather-api.glitch.me/api/current?lat=' + latitude + '&lon=' + longitude, true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            // Success!
+            var weatherData = JSON.parse(this.response);
+            console.log(weatherData);
+            displayCurrentWeather(weatherData);
         } else {
-            alert('Geolocation is not supported in your browser');
+            // We reached our target server, but it returned an error
         }
-    }
-    // in case the document is already rendered
-    if (document.readyState != 'loading') run();
-    // modern browsers
-    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', run());
-    // IE <= 8
-    else document.attachEvent('onreadystatechange', function () {
-        if (document.readyState == 'complete') run();
-    });
+    };
+    request.onerror = function () {
+        // There was a connection error of some sort
+    };
+    request.send();
+}
 
-    window.onload = function () {
-        document.getElementById("toggleTemp").addEventListener("click", toggleTempUnit, false);
+function usePosition(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    getCurrentWeather(latitude, longitude);
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
     }
+}
+
+// JS FOR PAGE LOAD - retrieve HTML5 geolocation// 
+
+function run() {
+    if (navigator.geolocation) {
+        // Get the user's current position
+        navigator.geolocation.getCurrentPosition(usePosition, showError);
+    } else {
+        alert('Geolocation is not supported in your browser');
+    }
+}
+// in case the document is already rendered
+if (document.readyState != 'loading') run();
+// modern browsers
+else if (document.addEventListener) document.addEventListener('DOMContentLoaded', run());
+// IE <= 8
+else document.attachEvent('onreadystatechange', function () {
+    if (document.readyState == 'complete') run();
+});
+
+window.onload = function () {
+    document.getElementById("toggleTemp").addEventListener("click", toggleTempUnit, false);
+}
