8911987e09e77179cb28a10a54baf45cfdca0c85
vlb834 <32312712+vlb834@users.noreply.github.com>
2018-01-27 22:25:04 +0000

    Added scripting for local city weather - need to fix sunset/rise time zones

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let tempUnit = "&#176C";
 let temp = 0;
 let latitude = '';
 let longitude = '';
+let city = '';
 
 function toggleTempUnit() {
     if (tempUnit === "&#176C") {
@@ -36,7 +37,7 @@ function detailedWeatherDescription(weather) {
         return weatherLocal = weatherLocal.slice(0, weatherLocal.length - 2) + '.';
     } else {
         weatherLocal = weather[0]['description'];
-        weatherLocal = weatherLocal.charAt(0).toUpperCase() + weather.slice(1);
+        weatherLocal = weatherLocal.charAt(0).toUpperCase() + weatherLocal.slice(1);
         return weatherLocal;
     }
 }
@@ -50,7 +51,7 @@ function windDegtoCardinalDirection(degrees) {
     return arr[Math.abs(val)];
 }
 
-function daylight(sunData) {
+function daylight(sunData) { //set time zone for CITIES
     let time = new Date(sunData * 1000);
     return time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
 }
@@ -62,17 +63,19 @@ function daylight(sunData) {
 
 function defineWeatherVariables(weatherData) {
     geolocation = weatherData['name'];
+    if (city !== "") {
+        geolocation = city;
+    }
     temp = Math.round(weatherData['main']['temp']);
     detailedDescription = detailedWeatherDescription(weatherData['weather']);
     humidity = 'Humidity: ' + weatherData['main']['humidity'] + '&#37';
     pressure = 'Pressure: ' + Math.round(weatherData['main']['pressure']) + ' hPa';
     windSpeed = 'Wind: ' + weatherData['wind']['speed'] + 'km/hr';
     windDirection = windDegtoCardinalDirection(weatherData['wind']['deg']);
-    sunRise = daylight(weatherData['sys']['sunrise']);
-    sunSet = daylight(weatherData['sys']['sunset']);
+    sunRise = daylight(weatherData['sys']['sunrise']); // need to set time zone!! 
+    sunSet = daylight(weatherData['sys']['sunset']); // need to set time zone!! 
     colorTemp = 'blue';
     colorWeather = 'grey';
-    // assignColors();
     gradient = 'linear-gradient(45deg, ' + colorTemp + ', ' + colorWeather + ')';
 }
 
@@ -91,9 +94,9 @@ function displayIcons(weather) {
             }
         }
     } else {
-        newIcon.src = weather[0]['icon']; 
+        newIcon.src = weather[0]['icon'];
         document.getElementById('icon').appendChild(newIcon);
-    } 
+    }
 }
 
 function displayCurrentWeather(weatherData) {
@@ -156,16 +159,16 @@ function showError(error) {
     }
 }
 
-function nyc() {
-    latitude = 40.4;
-    longitude = -73.56;
-    getCurrentWeather(latitude, longitude);
-}
-function la() {
-    latitude = 34.04;
-    longitude = -118.15;
-    getCurrentWeather(latitude, longitude);
-}
+function nyc() { latitude = 40.71278; longitude = -74.00597; getCurrentWeather(latitude, longitude); city = 'New York'; }
+function la() { latitude = 34.05223; longitude = -118.24368; getCurrentWeather(latitude, longitude); city = 'Los Angeles'; }
+function rio() { latitude = -22.90685; longitude = -43.17290; getCurrentWeather(latitude, longitude); city = 'Rio de Janeiro';  }
+function madrid() { latitude = 40.41678; longitude = -3.70379; getCurrentWeather(latitude, longitude); city = 'Madrid'; }
+function berlin() { latitude = 52.52001; longitude = 13.40495; getCurrentWeather(latitude, longitude); city = 'Berlin'; }
+function cairo() { latitude = 30.04442; longitude = 31.23571; getCurrentWeather(latitude, longitude); city = 'Cairo'; }
+function bangkok() { latitude = 13.75633; longitude = 100.50177; getCurrentWeather(latitude, longitude); city = 'Bangkok'; }
+function sydney() { latitude = -33.86882; longitude = 151.20930; getCurrentWeather(latitude, longitude); city = 'Sydney';  }
+function north() { latitude = 90.00; longitude = 0; getCurrentWeather(latitude, longitude); city = 'North Pole'}
+function south() { latitude = -90.00; longitude = 0; getCurrentWeather(latitude, longitude); city = 'South Pole'; }
 
 // JS FOR PAGE LOAD - retrieve HTML5 geolocation// 
 
@@ -190,20 +193,12 @@ window.onload = function () {
     document.getElementById("toggleTemp").addEventListener("click", toggleTempUnit, false);
     document.getElementById("ny").addEventListener("click", nyc, false);
     document.getElementById("la").addEventListener("click", la, false);
-
-
+    document.getElementById("rio").addEventListener("click", rio, false);
+    document.getElementById("madrid").addEventListener("click", madrid, false);
+    document.getElementById("berlin").addEventListener("click", berlin, false);
+    document.getElementById("cairo").addEventListener("click", cairo, false);
+    document.getElementById("bangkok").addEventListener("click", bangkok, false);
+    document.getElementById("sydney").addEventListener("click", sydney, false);
+    document.getElementById("north").addEventListener("click", north, false);
+    document.getElementById("south").addEventListener("click", south, false);
 }
-
-/*
-40°40′N	73°56′W	New York City
-34°03′N	118°15′W	Los Angeles
-22°55′S	43°12′W	Rio de Janeiro
-40°23′N	3°43′W	Madrid
-52°31′N	13°23′E	Berlin
-30°03′N	31°14′E	Cairo
-13°45′N	100°28′E	Bangkok
-33°52′S	151°13′E	Sydney
-
-
-
-*/
